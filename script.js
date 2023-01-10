cd2e442255a2747a7215c20ddce5c81edcfe63c2
unknown <aguilera.k02@gmail.com>
2023-01-10 12:51:28 -0800

    changed answer prompts to include arrays

--- script.js
+++ script.js
@@ -5,23 +5,27 @@ var buttonsSection = document.getElementById("buttons");
 var questions = [
     {
         question: "Commonly used data types DO NOT include _______.",
-        answer: "Alerts" //Other answers: Strings, Booleans, Numbers
+        answer: ["Alerts", "Strings", "Booleans", "Numbers"],
+        correct: buttonA,
     },
     {
         question: "The condition in an 'if / else' statement is enclosed with _______.",
-        answer: "Parenthesis" //Other answers: Quotes, Curly Brackets, Square Brackets
+        answer: ["Quotes", "Parenthesis", "Curly Brackets", "Square Brackets"],//"Parenthesis" Other answers: Quotes, Curly Brackets, Square Brackets,
+        correct: buttonB
     },
     {
         question:"Arrays in JavaScript can be used to store _______.",
-        answer: "All of the above" //Other answers: numbers and strings, other arrays, booleans
+        answer: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the above"],//"All of the above" Other answers: numbers and strings, other arrays, booleans
+        correct: buttonD
     },
     {
         question:"String values must be enclosed within _______ when being assigned to variables.",
-        answer: "Quotes" //Other answers: Commas, Curly Brackets, Parenthesis
+        answer: ["Quotes", "Commas", "Curly Brackets", "Parenthesis"],//"Quotes" Other answers: Commas, Curly Brackets, Parenthesis
+        correct: buttonA
     },
     {
         question:"A very useful tool used during development and debuggin for printing content to the debugger is _______.", 
-        answer: "console.log" //Other answers: JavaScript, terminal/bash, for loops
+        answer: ["JavaScript", "terminal/bash", "console.log", "for loop"]//"console.log" Other answers: JavaScript, terminal/bash, for loops
     }
 ]
 
@@ -39,16 +43,21 @@ function startQuiz () {
 
     instructions.textContent = "";
     startButton.setAttribute("style", "visibility:hidden");
+
+    mainTitle.textContent = questions[0].question;
     
     var buttonA = document.createElement("button");
     var buttonB = document.createElement("button");
     var buttonC = document.createElement("button");
     var buttonD = document.createElement("button");
 
-    buttonA.textContent = questions[0].question;
-    buttonB.textContent = "This is";
-    buttonC.textContent = "A button";
-    buttonD.textContent = "Hopefully";
+
+    // Write a for loop when I know what's happening
+
+    buttonA.textContent = questions[0].answer[0];
+    buttonB.textContent = questions[0].answer[1];
+    buttonC.textContent = questions[0].answer[2];
+    buttonD.textContent = questions[0].answer[3];
 
     buttonsSection.appendChild(buttonA);
     buttonsSection.appendChild(buttonB)

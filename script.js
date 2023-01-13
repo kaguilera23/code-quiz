c6b15def9cd9e7fb137a4b15735839b121985d4c
unknown <aguilera.k02@gmail.com>
2023-01-12 21:24:59 -0500

    initials show up in local storage

--- script.js
+++ script.js
@@ -2,11 +2,10 @@ var mainTitle = document.querySelector("h2");
 var instructions = document.getElementById("instructions");
 var startButtonSection = document.getElementById("start-section");
 var mcButtonsSection = document.getElementById("choice-section");
-var timeLeft = document.getElementById("time-left");
-// var choiceA = document.getElementById("button-a");
-// var choiceB = document.getElementById("button-b");
-// var choiceC = document.getElementById("button-c");
-// var choiceD = document.getElementById("button-d");
+var timeSection = document.getElementById("time-left");
+var inputSection = document.getElementById("input-section");
+
+var timeLeft = 20;
 
 // Main Page Content
 mainTitle.textContent = "Coding Quiz Challenge";
@@ -16,17 +15,34 @@ startButton.textContent = "Start Quiz";
 startButton.setAttribute("id", "start-button");
 startButtonSection.appendChild(startButton);
 
-// When the user starts the quiz
+// When the user clicks to start the quiz
 startButtonSection.addEventListener("click", startQuiz)
 
-function startQuiz () {
-    // Get rid of title and instruction
+function startQuiz() {
+
+// Establishes Timer for Quiz
+
+function timer() {
+    var timerInterval = setInterval(function(){
+        timeSection.textContent = timeLeft + " seconds";
+
+        if (timeLeft <= 0) {
+            clearInterval(timerInterval);
+        }
+
+        timeLeft--;
+
+    }, 1000)
+}
+
+timer();
+
+// Removes instructions and start button
 
     instructions.textContent = "";
     startButton.remove();
 
-    // Create multiple choice buttons    
-    
+// Created multiple choice buttons
     var buttonA = document.createElement("button");
     var buttonB = document.createElement("button");
     var buttonC = document.createElement("button");
@@ -47,126 +63,147 @@ function startQuiz () {
     mcButtonsSection.appendChild(buttonC);
     mcButtonsSection.appendChild(buttonD);
 
-    var questions = [
-        {
-            question: "Commonly used data types DO NOT include _______.",
-            answers: ["Alerts", "Strings", "Booleans", "Numbers"],
-            correct: buttonA
-        },
-        {
-            question: "The condition in an 'if / else' statement is enclosed with _______.",
-            answers: ["Quotes", "Parenthesis", "Curly Brackets", "Square Brackets"],//"Parenthesis" Other answers: Quotes, Curly Brackets, Square Brackets,
-            correct: buttonB,
-        },
-        {
-            question:"Arrays in JavaScript can be used to store _______.",
-            answers: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the above"],//"All of the above" Other answers: numbers and strings, other arrays, booleans
-            correct: buttonD,
-        },
-        {
-            question:"String values must be enclosed within _______ when being assigned to variables.",
-            answers: ["Quotes", "Commas", "Curly Brackets", "Parenthesis"],//"Quotes" Other answers: Commas, Curly Brackets, Parenthesis
-            correct: buttonA,
-        },
-        {
-            question:"A very useful tool used during development and debuggin for printing content to the debugger is _______.", 
-            answers: ["JavaScript", "terminal/bash", "console.log", "for loop"],//"console.log" Other answers: JavaScript, terminal/bash, for loops
-            correct: buttonC,
+function firstQuestion () {
+    var questionOne = {
+        question: "Commonly used data types DO NOT include _______.",
+        answers: ["Alerts", "Strings", "Booleans", "Numbers"],
+        correct: buttonA
+    }
+    mainTitle.textContent = questionOne.question;
+
+        buttonA.textContent = questionOne.answers[0];
+        buttonB.textContent = questionOne.answers[1];
+        buttonC.textContent = questionOne.answers[2];
+        buttonD.textContent = questionOne.answers[3];
+
+    mcButtonsSection.addEventListener("click", function(answer){
+        if(answer.target === questionOne.correct) {
+            console.log("correct");
+            endScore();
+        } else {
+            console.log("incorrect");
+            timeLeft-= 10;
+            endScore();
         }
-    ]
-
-    // function hello (answer) {
-    //                if (answer.target === questions[0].correct) {
-    //             console.log("correct");
-    //         } else {
-    //             console.log("incorrect");
-    //         }
-    //     };
-       
-   
-    var questionOne = function () {
-        mainTitle.textContent = questions[0].question;
-
-        buttonA.textContent = questions[0].answers[0];
-        buttonB.textContent = questions[0].answers[1];
-        buttonC.textContent = questions[0].answers[2];
-        buttonD.textContent = questions[0].answers[3];
-
-        mcButtonsSection.addEventListener("click", function(){console.log("Hello")});
-    }
-  
-    function questionTwo () {
-        mainTitle.textContent = questions[1].question;
-
-        buttonA.textContent = questions[1].answers[0];
-        buttonB.textContent = questions[1].answers[1];
-        buttonC.textContent = questions[1].answers[2];
-        buttonD.textContent = questions[1].answers[3];
-
-        // mcButtonsSection.addEventListener("click", function hello (answer) {
-        //     if (answer.target === questions[1].correct) {
-        //         console.log("correct");
-        //     } else {
-        //         console.log("incorrect");
-        //     }
-    // });
-    }
-
-    function questionThree () {
-        mainTitle.textContent = questions[2].question;
-
-        buttonA.textContent = questions[2].answers[0];
-        buttonB.textContent = questions[2].answers[1];
-        buttonC.textContent = questions[2].answers[2];
-        buttonD.textContent = questions[2].answers[3];
-
-    //     mcButtonsSection.addEventListener("click", function hello (answer) {
-    //         if (answer.target === questions[2].correct) {
-    //             console.log("correct");
-    //         } else {
-    //             console.log("incorrect");
-    //         }
-    // });
-
-    };
-
-    function questionFour () {
-        mainTitle.textContent = questions[3].question;
-
-        buttonA.textContent = questions[3].answers[0];
-        buttonB.textContent = questions[3].answers[1];
-        buttonC.textContent = questions[3].answers[2];
-        buttonD.textContent = questions[3].answers[3];
-
-    //     mcButtonsSection.addEventListener("click", function hello (answer) {
-    //         if (answer.target === questions[3].correct) {
-    //             console.log("correct");
-    //         } else {
-    //             console.log("incorrect");
-    //         }
-    // });
-
-    };
-
-    function questionFive () {
-        mainTitle.textContent = questions[4].question;
-
-        buttonA.textContent = questions[4].answers[0];
-        buttonB.textContent = questions[4].answers[1];
-        buttonC.textContent = questions[4].answers[2];
-        buttonD.textContent = questions[4].answers[3];
-
-    //     mcButtonsSection.addEventListener("click", function hello (answer) {
-    //         if (answer.target === questions[4].correct) {
-    //             console.log("correct");
-    //         } else {
-    //             console.log("incorrect");
-    //         }
-    // });
-    };
-console.log(questionOne);
-questionTwo;
-questionThree;
-questionFour;
-questionFive;
+    });
 }
+
+function secondQuestion() {
+    var questionTwo = {
+        question: "The condition in an 'if / else' statement is enclosed with _______.",
+             answers: ["Quotes", "Parenthesis", "Curly Brackets", "Square Brackets"],//"Parenthesis" Other answers: Quotes, Curly Brackets, Square Brackets,
+             correct: buttonB,
+    }
+
+    mainTitle.textContent = questionTwo.question;
+
+    buttonA.textContent = questionTwo.answers[0];
+    buttonB.textContent = questionTwo.answers[1];
+    buttonC.textContent = questionTwo.answers[2];
+    buttonD.textContent = questionTwo.answers[3]; 
+}
+
+function thirdQuestion() {
+    var questionThree = {
+        question:"Arrays in JavaScript can be used to store _______.",
+             answers: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the above"],//"All of the above" Other answers: numbers and strings, other arrays, booleans
+             correct: buttonD,
+    }
+
+    mainTitle.textContent = questionThree.question;
+
+    buttonA.textContent = questionThree.answers[0];
+    buttonB.textContent = questionThree.answers[1];
+    buttonC.textContent = questionThree.answers[2];
+    buttonD.textContent = questionThree.answers[3];   
+}
+
+function fourthQuestion() {
+    var questionFour = {
+        question:"String values must be enclosed within _______ when being assigned to variables.",
+             answers: ["Quotes", "Commas", "Curly Brackets", "Parenthesis"],//"Quotes" Other answers: Commas, Curly Brackets, Parenthesis
+             correct: buttonA,
+    }
+
+    mainTitle.textContent = questionFour.question;
+
+    buttonA.textContent = questionFour.answers[0];
+    buttonB.textContent = questionFour.answers[1];
+    buttonC.textContent = questionFour.answers[2];
+    buttonD.textContent = questionFour.answers[3];   
+}
+
+function fifthQuestion() {
+    var questionFive = {
+        question:"A very useful tool used during development and debuggin for printing content to the debugger is _______.", 
+             answers: ["JavaScript", "terminal/bash", "console.log", "for loop"],//"console.log" Other answers: JavaScript, terminal/bash, for loops
+             correct: buttonC,
+    }
+
+    mainTitle.textContent = questionFive.question;
+
+    buttonA.textContent = questionFive.answers[0];
+    buttonB.textContent = questionFive.answers[1];
+    buttonC.textContent = questionFive.answers[2];
+    buttonD.textContent = questionFive.answers[3];   
+}
+
+firstQuestion();
+
+function endScore() {
+    
+    mainTitle.textContent = "All Done!";
+    instructions.textContent = "Your final score is " + timeLeft;
+    mcButtonsSection.remove();
+
+    var enterInitials = document.createElement("div");
+    var initialsSection = document.createElement("input");
+    var submitButton = document.createElement("button");
+    submitButton.setAttribute("id", "submit-button");
+    enterInitials.setAttribute("id", "enter-initials");
+    initialsSection.setAttribute("id", "initials-here");
+
+    enterInitials.textContent = "Enter initials:";
+    submitButton.textContent = "Submit"
+
+    inputSection.appendChild(enterInitials);
+    inputSection.appendChild(initialsSection);
+    inputSection.appendChild(submitButton);
+
+    var names = [];
+    
+    function storeData () {
+        localStorage.setItem("name", JSON.stringify(names));
+    };
+
+    inputSection.addEventListener("keydown", function(event){
+        console.log(event)
+
+        var namesText = initialsSection.value;
+
+        if (event.key == "Enter") {
+            names.push(namesText);
+            initialsSection.value = "";
+            console.log("clicked enter");
+            storeData(); 
+        }
+    });
+
+    inputSection.addEventListener("click", function(event) {
+        console.log(event);
+
+        var namesText = initialsSection.value;
+
+        if (event.target === submitButton) {
+            names.push(namesText);
+            initialsSection.value = "";
+            console.log("clicked");
+            storeData();
+        }
+    });
+
+   
+    
+}
+
+};
--- script2.js
+++ script2.js
@@ -1,5 +1,12 @@
 var goBack = document.getElementById("go-back")
+var highscores = document.getElementById("highscores");
+
+var names = localStorage.getItem("name");
+
+console.log(names);
 
 goBack.addEventListener("Click", function () {
     goBack.setAttribute("href", "index.html");
 });
+
+
